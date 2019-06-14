779646df7bf3bf2fa078ae787b8f2991ceac3100
Adrienne <adriennenking@gmail.com>
2019-06-14 16:11:09 -0700

    Assgn work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -15,4 +15,18 @@ describe("routes : static", () => {
       })
     })
   })
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200 and have 'Marco' in the body of the response", (done) => {
+
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Marco");
+        
+        done();
+      })
+    })
+  })
+  })
 })
