4371981618e1840292065444e13b4084f50efc47
Adrienne <adriennenking@gmail.com>
2019-06-16 22:11:43 -0700

    Updated Marco

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,16 @@ const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
 
+describe("Get /marco", () => {
+  it("should return status code and have 'Marco' in the body of the response", (done) => {
+
+    request.get(base, (err, res, body) => {
+      expect(res.statusCode).toBe(200);
+      expect(body).toContain("Polo");
+
+      done();
+    })
+  })
+})
 
 module.exports = router;
