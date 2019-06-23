ffd90d82b7482a9b2d5d7fca9c05cd27dd30beb4
Adrienne <adriennenking@gmail.com>
2019-06-23 13:05:43 -0700

    For review

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const topicRouters = require("../routes/topics");
+
     app.use(staticRoutes);
+    app.use(topicRoutes);
   }
 }
