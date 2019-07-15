e21ac73d122db6d332725fe42ca1b431bec91196
Adrienne <adriennenking@gmail.com>
2019-07-14 17:27:12 -0700

    Updated spec

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -7,7 +7,7 @@ const Topic = require("../../src/db/models").Topic;
 describe("routes : topics", () => {
 
   beforeEach((done) => {
-    this.Topic;
+    this.topic;
     sequelize.sync({force: true}).then((res) => {
 
       Topic.create({
