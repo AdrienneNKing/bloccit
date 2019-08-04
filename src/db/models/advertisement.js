1b121fda9995f5e7bd4ea401c5c6ec46fcf7f567
Adrienne <adriennenking@gmail.com>
2019-08-04 15:56:27 -0700

    Sequelize review

---
+++ src/db/models/advertisement.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Advertisement = sequelize.define('Advertisement', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  Advertisement.associate = function(models) {
+    Advertisement.hasMany(models.Banner, {
+      foreignKey: "advertisementId",
+      as: "banners",
+    });
+    Advertisement.hasMany(models.Rule, {
+      foreignKey: "advertisementId",
+      as: "rules",
+    });
+  };
+  return Advertisement;
+};
---
+++ src/db/queries.advertisements.js
@@ -0,0 +1,69 @@
+const Advertisement = require("./models").Advertisement;
+
+module.exports = {
+
+  getAllAdvertisements(callback){
+    return Advertisement.all()
+
+
+    .then((topics) => {
+      callback(null, topics);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  addAdvertisement(newAdvertisement, callback){
+      return Advertisement.create({
+        title: newAdvertisement.title,
+        description: newAdvertisement.description
+      })
+      .then((advertisement) => {
+        callback(null, advertisement;
+      })
+      .catch((err) => {
+        callback(err);
+      })
+    },
+    getTopic(id, callback){
+     return Advertisement.findById(id)
+     .then((advertisement) => {
+       callback(null, advertisement);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+   deleteAdvertisement(id, callback){
+     return Advertisement.destroy({
+       where: {id}
+     })
+     .then((advertisement) => {
+       callback(null, advertisement);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+
+   updateAdvertisement(id, updatedTopic, callback){
+     return Advertisement.findById(id)
+     .then((advertisement) => {
+       if(!advertisement){
+         return callback("Advertisement not found");
+       }
+
+//#1
+      advertisement.update(updatedAdvertisement, {
+         fields: Object.keys(updatedAdvertisement)
+       })
+       .then(() => {
+         callback(null, advertisement);
+       })
+       .catch((err) => {
+         callback(err);
+       });
+     });
+   }
+
+}
