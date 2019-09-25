663bb17957af8d00ed6c23133b644ec99d34b88b
Adrienne <adriennenking@gmail.com>
2019-09-25 14:03:07 -0700

    Authentications and users

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,11 +5,13 @@ module.exports = {
     const advertisementRoutes = require("../routes/advertisements");
     const marco = require("../routes/marco-polo");
     const postRoutes = require("../routes/posts");
+    const userRoutes = require("../routes/users");
 
     app.use(marco);
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(advertisementRoutes);
     app.use(postRoutes);
+    app.use(userRoutes);
   }
 }
